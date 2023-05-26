45402e469ea8350fe4d23f837b5975127efdd63b
LHMAIME <mohamed.lhmaime@sogeti.com>
2023-05-26 12:46:00 +0200

    ajout des utilisateurs et authentification

--- src/main/java/com/example/hopital/HopitalApplication.java
+++ src/main/java/com/example/hopital/HopitalApplication.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Date;
 
@@ -39,6 +42,11 @@ public class HopitalApplication implements CommandLineRunner {
 //				.malade(true)
 //				.score(56)
 //				.build();
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder(){
+		return new BCryptPasswordEncoder();
 	}
 
 }
